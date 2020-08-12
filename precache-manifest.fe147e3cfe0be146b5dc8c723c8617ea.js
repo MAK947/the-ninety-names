9aad5e4eb407d04a1aae44fd75fbc8914733695c
Maaz Ahmad Khan <mak94723@gmail.com>
2020-08-12 14:13:46 +0530

    Updates

---
+++ precache-manifest.fe147e3cfe0be146b5dc8c723c8617ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac3a1589dd70aed21808d973daee000",
+    "url": "/the-ninety-names/index.html"
+  },
+  {
+    "revision": "92de6a1f1762f8743c2c",
+    "url": "/the-ninety-names/static/css/2.cc2d33b2.chunk.css"
+  },
+  {
+    "revision": "d0e3f8eb10f04540b93a",
+    "url": "/the-ninety-names/static/css/main.0fc1b995.chunk.css"
+  },
+  {
+    "revision": "92de6a1f1762f8743c2c",
+    "url": "/the-ninety-names/static/js/2.0c14b985.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/the-ninety-names/static/js/2.0c14b985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e3f8eb10f04540b93a",
+    "url": "/the-ninety-names/static/js/main.4be8ecca.chunk.js"
+  },
+  {
+    "revision": "8a54deacf0e069b09079",
+    "url": "/the-ninety-names/static/js/runtime-main.ff3543b7.js"
+  },
+  {
+    "revision": "57502ab1bedd3acf45e40b8f476939df",
+    "url": "/the-ninety-names/static/media/AAAGoldenLotus Stg1_Ver1 Regular.57502ab1.ttf"
+  },
+  {
+    "revision": "d7baf1b48be705c1354f690797a23a1d",
+    "url": "/the-ninety-names/static/media/UTM Beautiful Caps.d7baf1b4.woff"
+  }
+]);
